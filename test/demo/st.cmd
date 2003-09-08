30861677432c446d3d0c06e32babb37ff5d82851
Andrew Johnson <anj@aps.anl.gov>
2003-09-08 17:38:30 +0000

    R3.14.3 changes (registerRecordDeviceDriver rename).

--- test/demo/st.cmd
+++ test/demo/st.cmd
@@ -1,6 +1,6 @@
 ld < ../../bin/vxWorks-68040/demoLibrary.munch
 dbLoadDatabase "../../dbd/demo.dbd"
-registerRecordDeviceDriver(pdbbase)
+demo_registerRecordDeviceDriver(pdbbase)
 dbLoadRecords "demo.db"
 iocInit
 seq &demo
