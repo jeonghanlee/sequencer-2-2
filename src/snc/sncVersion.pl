ee16b8388678b34015a2e1d7ffbfa073d094b8de
W. Eric Norum <wenorum@lbl.gov>
2004-03-03 15:53:13 +0000

    Use self-contained Perl scripts to create version files.

--- src/snc/sncVersion.pl
+++ src/snc/sncVersion.pl
@@ -1,12 +1,10 @@
-#!/bin/bin/perl
+#!/usr/bin/perl
 #
 # sncVersion - create the snc version module
 #
 
 $version = $ARGV[0];
-$date = `date`;
-chomp($date);
-#($sec,$min,$hour,$mday,$mon,$year) = localtime(time);
+$now = localtime;
 print "/* sncVersion.c - version & date */\n";
 print "/* Created by sncVersion.pl */\n";
-print "char *sncVersion = \"SNC Version ${version}: ${date}\";\n";
+print "char *sncVersion = \"SNC Version ${version}: ${now}\";\n";
