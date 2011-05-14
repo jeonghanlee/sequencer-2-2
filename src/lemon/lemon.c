d035b8a1b156f92774ee37a0b9e460bb434ad3e0
ben.franksen <ben.franksen@online.de>
2011-05-14 12:39:31 +0000

    re-apply patch: make lemon accept conflicts (we have 3)

--- src/lemon/lemon.c
+++ src/lemon/lemon.c
@@ -1538,7 +1538,7 @@ int main(int argc, char **argv)
   }
 
   /* return 0 on success, 1 on failure. */
-  exitcode = ((lem.errorcnt > 0) || (lem.nconflict > 0)) ? 1 : 0;
+  exitcode = ((lem.errorcnt > 0)) ? 1 : 0;
   successful_exit = (exitcode == 0);
   exit(exitcode);
   return (exitcode);
