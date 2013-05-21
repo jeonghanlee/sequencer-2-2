e2bd4140d4fbbbfc9611d864b90b73c6a640841f
benjamin.franksen <benjamin.franksen@helmholtz-berlin.de>
2013-05-21 11:13:36 +0000

    docs: some html layout improvements

--- documentation/conf.py
+++ documentation/conf.py
@@ -116,7 +116,7 @@ html_theme_path = ["themes"]
 html_title = "EPICS Sequencer Home"
 
 # A shorter title for the navigation bar.  Default is the same as html_title.
-html_short_title = "Home"
+#html_short_title = "Home"
 
 # The name of an image file (relative to this directory) to place at the top
 # of the sidebar.
