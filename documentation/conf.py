f1bad4b4a8cc9084b36b9f633deb1f3216211559
Ben Franksen <benjamin.franksen@helmholtz-berlin.de>
2015-01-20 10:36:16 +0000

    docs: make the sequencer version more prominent on the web page

--- documentation/conf.py
+++ documentation/conf.py
@@ -123,7 +123,7 @@ html_theme_path = ["themes"]
 
 # The name for this set of Sphinx documents.  If None, it defaults to
 # "<project> v<release> documentation".
-html_title = "EPICS Sequencer Home"
+html_title = "EPICS Sequencer Version %s" % version
 
 # A shorter title for the navigation bar.  Default is the same as html_title.
 #html_short_title = "Home"
