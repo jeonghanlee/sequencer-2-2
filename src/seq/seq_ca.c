20e4d1fb9343634445e83d1a31d9e94264f561fa
Ben Franksen <benjamin.franksen@helmholtz-berlin.de>
2015-04-01 10:53:08 +0000

    seq: fixed a bad assertion in proc_db_events

--- src/seq/seq_ca.c
+++ src/seq/seq_ca.c
@@ -276,34 +276,31 @@ static void proc_db_events(
 			);
 		}
 	}
-	else
+	else if (value != NULL)
 	{
-		/* Copy value and meta data into user variable shared buffer
-		   (can get NULL value pointer for put completion only) */
-		assert((value==NULL)==(evtype == pvEventPut));
-		if (value != NULL)
+		/* Copy value and meta data into user variable shared buffer */
+		void *val = pv_value_ptr(value,type);
+		PVMETA meta;
+
+		/* put completion always has NULL value pointer */
+		assert(evtype != pvEventPut);
+		/* must not use an initializer here, the MS C compiler chokes on it */
+		meta.timeStamp = pv_stamp(value,type);
+		meta.status = pv_status(value,type);
+		meta.severity = pv_severity(value,type);
+		meta.message = NULL;
+
+		/* Set error message only when severity indicates error */
+		if (meta.severity != pvSevrNONE)
 		{
-			void *val = pv_value_ptr(value,type);
-			PVMETA meta;
-
-			/* must not use an initializer here, the MS C compiler chokes on it */
-			meta.timeStamp = pv_stamp(value,type);
-			meta.status = pv_status(value,type);
-			meta.severity = pv_severity(value,type);
-			meta.message = NULL;
-
-			/* Set error message only when severity indicates error */
-			if (meta.severity != pvSevrNONE)
-			{
-				const char *pmsg = pvVarGetMess(dbch->pvid);
-				if (!pmsg) pmsg = "unknown";
-				meta.message = pmsg;
-			}
-
-			/* Write value and meta data to shared buffers.
-			   Set the dirty flag only if this was a monitor event. */
-			ss_write_buffer(ch, val, &meta, evtype == pvEventMonitor);
+			const char *pmsg = pvVarGetMess(dbch->pvid);
+			if (!pmsg) pmsg = "unknown";
+			meta.message = pmsg;
 		}
+
+		/* Write value and meta data to shared buffers.
+		   Set the dirty flag only if this was a monitor event. */
+		ss_write_buffer(ch, val, &meta, evtype == pvEventMonitor);
 	}
 
 	epicsMutexMustLock(sp->lock);
