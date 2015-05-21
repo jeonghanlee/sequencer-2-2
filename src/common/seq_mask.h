94853883a56288870605f72c0beafa9a00dd184c
Ben Franksen <benjamin.franksen@helmholtz-berlin.de>
2015-05-21 15:07:03 +0000

    common: in seq_mask macros, add parens around words parameter

--- src/common/seq_mask.h
+++ src/common/seq_mask.h
@@ -8,8 +8,8 @@ typedef epicsUInt32 seqMask;				/* for event masks and options */
 #define NBITS			(8*sizeof(seqMask))	/* # bits in seqMask word */
 #define NWORDS(maxBitNum)	(1+(maxBitNum)/NBITS)	/* # words in seqMask */
 
-#define bitSet(words, bitnum)	( words[(bitnum)/NBITS] |=  (1u<<((bitnum)%NBITS)))
-#define bitClear(words, bitnum)	( words[(bitnum)/NBITS] &= ~(1u<<((bitnum)%NBITS)))
-#define bitTest(words, bitnum)	((words[(bitnum)/NBITS] &  (1u<<((bitnum)%NBITS))) != 0)
+#define bitSet(words, bitnum)	( (words)[(bitnum)/NBITS] |=  (1u<<((bitnum)%NBITS)))
+#define bitClear(words, bitnum)	( (words)[(bitnum)/NBITS] &= ~(1u<<((bitnum)%NBITS)))
+#define bitTest(words, bitnum)	(((words)[(bitnum)/NBITS] &  (1u<<((bitnum)%NBITS))) != 0)
 
 #endif	/*INCLseq_maskh*/
